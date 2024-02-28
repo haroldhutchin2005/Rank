aa99aec341be98612dcc2f86f823f69a53ecf42d
Harold Hutchins <160975590+haroldhutchin2005@users.noreply.github.com>
2024-02-28 10:37:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const knights = require('knights-canvas');
 const fs = require('fs').promises;
 
 const app = express();
-const port = process.env.PORT || 3000; // Choose any port you prefer
+const port = 3000; // Choose any port you prefer
 
-app.get('/rank', async (req, res) => {
+app.get('/generateRankImage', async (req, res) => {
   try {
-    const { id, name, thread, backgroundimage, needxp, currenexp, level } = req.query;
+    const { id, name, backgroundimage, needxp, currenexp, level } = req.query;
 
     // Fetch Facebook profile picture
     const facebookPicture = await axios.get(
@@ -16,26 +16,31 @@ app.get('/rank', async (req, res) => {
       { responseType: 'arraybuffer' }
     );
 
+    // Fetch background image
+    const backgroundImage = await axios.get(backgroundimage, { responseType: 'arraybuffer' });
+
     // Create Knights Canvas rank image
     const rankImage = await new knights.Rank()
       .setAvatar(`data:image/jpeg;base64,${Buffer.from(facebookPicture.data, 'binary').toString('base64')}`)
       .setUsername(name)
-      .setBg(backgroundimage)
+      .setBg(`data:image/jpeg;base64,${Buffer.from(backgroundImage.data, 'binary').toString('base64')}`)
       .setNeedxp(needxp)
       .setCurrxp(currenexp)
       .setLevel(level)
-      .setRank(thread)
+      .setRank('https://i.postimg.cc/SKFwkJqK/images-2024-02-28-T103459-576-removebg-preview.png')
       .toAttachment();
 
     const imageData = rankImage.toBuffer();
 
-    // Save both the rank image and profile picture to the same tmp folder
+    // Save both the rank image, profile picture, and background image to the same tmp folder
     const folderPath = './tmp/';
     const filePathRankImage = `${folderPath}${id}_srank.png`;
     const filePathProfilePicture = `${folderPath}${id}_profile.png`;
+    const filePathBackgroundImage = `${folderPath}${id}_background.png`;
 
     await fs.writeFile(filePathRankImage, imageData);
     await fs.writeFile(filePathProfilePicture, facebookPicture.data);
+    await fs.writeFile(filePathBackgroundImage, backgroundImage.data);
 
     // Send the rank image as an attachment in the response
     res.sendFile(filePathRankImage, { root: __dirname }, (err) => {
